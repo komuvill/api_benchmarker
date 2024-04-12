1611aec89f8ec441ef14d33702adb10425df0181
komuvill <v-v.komulainen@hotmail.com>
2024-04-12 14:57:00 +0300

    sample httpRequest function

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
-
+    "io"
+    "io/ioutil"
+    "net/http"
 	"github.com/spf13/cobra"
 )
 
@@ -14,8 +16,43 @@ func runBenchmark(cmd *cobra.Command, args []string) {
 	concurrency, _ := cmd.Flags().GetInt("concurrency")
 	duration, _ := cmd.Flags().GetInt("duration")
 
-	// TODO: Implement the benchmarking logic
 	fmt.Printf("Benchmarking %s with %s method, %d requests, %d concurrent requests, for %d seconds\n", url, method, requests, concurrency, duration)
+
+    //TODO: construct responseBody if possible
+
+    responseBody, statusCode, err := httpRequest(method, url, nil)
+
+    if err != nil {
+        fmt.Printf("%v\n", err)
+        return
+    }
+    
+    fmt.Printf("Response Status Code: %d\n", statusCode)
+    fmt.Printf("Response Body: %s\n", responseBody)
+}
+
+func httpRequest(method, url string, body io.Reader) (string, int, error) {
+    // Create a new request with the method, URL, and body if provided
+    req, err := http.NewRequest(method, url, body)
+    if err != nil {
+        return "", 0, fmt.Errorf("error creating request: %v", err)
+    }
+
+    // Send the request
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        return "", 0, fmt.Errorf("error making request: %v", err)
+    }
+    defer resp.Body.Close()
+
+    // Read the response body
+    respBody, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return "", resp.StatusCode, fmt.Errorf("error reading response body: %v", err)
+    }
+
+    return string(respBody), resp.StatusCode, nil
 }
 
 func validateFlags(cmd *cobra.Command, args []string) error {
@@ -45,6 +82,8 @@ func validateFlags(cmd *cobra.Command, args []string) error {
 		return fmt.Errorf("'%s' is not a valid HTTP method. Following HTTP methods are supported: GET, POST, PUT, DELETE", method)
 	}
 
+    //TODO: If method is POST/PUT/DELETE, make sure body is also supplied?
+
 	return nil
 }
 
@@ -69,6 +108,7 @@ func main() {
 	rootCmd.PersistentFlags().IntVarP(&requests, "requests", "r", 100, "The number of requests to perform.")
 	rootCmd.PersistentFlags().IntVarP(&concurrency, "concurrency", "c", 10, "The level of concurrency for the requests.")
 	rootCmd.PersistentFlags().IntVarP(&duration, "duration", "d", 10, "The duration of the test in seconds.")
+    //TODO: Flag for defining body for requests like POST
 
 	rootCmd.PreRunE = validateFlags
 
